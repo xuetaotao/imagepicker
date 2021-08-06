9f7a4f803691981be3f482c86772316cd81c8490
xuetaotao <xuetaotao@xgd.com>
2021-08-06 17:55:14 +0800

    create new project

---
+++ app/src/main/java/com/jlpay/imagepicker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jlpay.imagepicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
