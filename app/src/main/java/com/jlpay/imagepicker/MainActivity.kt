b4f14d798bc6265d9a03cc6989a4b0b7f8061eb1
xuetaotao <xuetaotao@xgd.com>
2021-08-13 16:07:36 +0800

    feat:添加测试用例

--- app/src/main/java/com/jlpay/imagepicker/MainActivity.kt
+++ app/src/main/java/com/jlpay/imagepicker/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.jlpay.imagepicker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var iv_image: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iv_image = findViewById(R.id.iv_image)
     }
+
+    fun imagePickerTest(view: View) {
+        ImagePicker.with(this)
+            .imagePickerListener(object : ImagePicker.ImagePickerListener {
+                override fun onFailed(msg: String, code: String) {
+                    Toast.makeText(this@MainActivity, msg + code, Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onSuccess(imagePath: String) {
+                    val bitmap = BitmapFactory.decodeFile(imagePath)
+                    iv_image.setImageBitmap(bitmap)
+                }
+            })
+            .compress(true)
+            .crop(false)
+            .isCamera(false)
+            .startPick()
+    }
+
+
 }
